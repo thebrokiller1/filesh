5c29f266813d4c508da460f2569517b228dfb5cf
thebrokiller1 <fabian.marktler0@gmail.com>
2024-07-20 16:13:36 +0200

    Basic Bildergalerie

--- src/app/components/MainInput.tsx
+++ src/app/components/MainInput.tsx
@@ -2,10 +2,12 @@
 
 import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
 
-export function LibraryInput() {
+export function MainInput() {
   const placeholders = [
-    "@ArnoldSchwarzenegger#BackStage",
-    "@LamineYamal#EM-2024"
+    "@Lamine-Yamal:EM-2024",
+    "Max-Mustermann:Hochzeit-2021",
+    "Hans-Müller:Urlaub-Karibik",
+    "Ninja:FN-Winstream",
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,8 +19,8 @@ export function LibraryInput() {
   };
   return (
     <div className="h-[40rem] flex flex-col justify-center  items-center px-4">
-      <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-yellow-500 opacity-70">
-        Gib hier deinen Galerie-Code ein:
+      <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-black">
+        Such nach deiner Bildergalerie:
       </h2>
       <PlaceholdersAndVanishInput
         placeholders={placeholders}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import { FindLibrary } from "./components/FindLibrary";
-import Image from "next/image";
-import bgImage from "@/bg-image.jpg"
+import { MainInput } from "./components/MainInput";
 
 export default function Home() {
   return (
-    <main className=" flex justify-center">
-      <Image
-      src={bgImage}      
-      fill={true}
-      objectFit="cover"
-      alt="Background-Image"
-      />
-      <FindLibrary></FindLibrary>
+    <main className="flex justify-center">
+      <MainInput></MainInput>
     </main>
   );
 }
